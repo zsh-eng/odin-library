14593fb6776a94533651867cc92ec82410405519
sheng <49238630+zsh-eng@users.noreply.github.com>
2022-04-15 21:46:50 +0800

    refactor: Change implementation of Book from object to class

--- book.js
+++ book.js
@@ -1,10 +1,13 @@
-function Book(title, author, pages, read) {
-	this.title = title
-	this.author = author
-	this.pages = pages
-	this.read = read
-	this.info = () =>
-		`${title} by ${author}, ${pages} pages, ${read ? "read" : "not read yet"}`
+class Book {
+	constructor(title, author, pages, read) {
+		this.title = title
+		this.author = author
+		this.pages = pages
+		this.read = read
+	}
+	info() {
+		return `${title} by ${author}, ${pages} pages, ${read ? "read" : "not read yet"}`
+	}
 }
 
 function addBookToLibrary() {}
